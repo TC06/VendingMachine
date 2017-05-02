ae495df8ad0ad9292cb76ddbe19397e4bb12097d
Harun Abdullah <meedodk@Haruns-MacBook-Pro.local>
2017-05-02 11:23:18 +0200

    Rav på min pc
    Commuter

---
+++ dk.dtu.compute.mbse.tutorial.yawl/src/dk/dtu/compute/mbse/yawl/constraints/StartEndPlaces.java
@@ -0,0 +1,69 @@
+package dk.dtu.compute.mbse.yawl.constraints;
+
+import java.util.Iterator;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.IValidationContext;
+import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
+
+import dk.dtu.compute.mbse.yawl.PType;
+import dk.dtu.compute.mbse.yawl.Place;
+import dk.dtu.compute.mbse.yawl.YAWLNet;
+import dk.dtu.compute.mbse.yawl.functions.YAWLFunctions;
+
+// imports missing
+
+/**
+ * This is a constraint saying that a YAWL net should have exactly one start
+ * place and one end place.
+ *
+ * @author ekki@dtu.dk
+ * @generated NOT
+ */
+public class StartEndPlaces extends AbstractModelConstraint {
+
+	public IStatus validate(IValidationContext ctx) {
+		EObject object = ctx.getTarget();
+
+		// TODO YAWLNet must refer to your java class for YAWL nets
+		if (object instanceof YAWLNet) {
+			EObject container = object.eContainer();
+			if (container instanceof PetriNet) {
+				int startCount = 0;
+				int endCount = 0;
+				Iterator<EObject> iterator = container.eAllContents();
+				while (iterator.hasNext()) {
+					EObject content = iterator.next();
+
+					// TODO count number of places that are start places
+					// and number of places that are end places
+					if (content instanceof Place) {
+						Place place = (Place) content;
+						PType type = YAWLFunctions.getType(place);
+						if (type.equals(PType.START)) {
+							startCount++;
+							if (startCount > 1) {
+								break;
+							}
+						} else if (type.equals(PType.FINISH)) {
+							endCount++;
+							if (endCount > 1) {
+								break;
+							}
+						}
+					}
+				}
+				if (startCount != 1 || endCount != 1) {
+					// if there is not exactly one start place and exactly one
+					// end place, return a failure status.
+					return ctx.createFailureStatus(new Object[] { container });
+				}
+			}
+		}
+		// otherwise return a success status
+		return ctx.createSuccessStatus();
+	}
+
+}
