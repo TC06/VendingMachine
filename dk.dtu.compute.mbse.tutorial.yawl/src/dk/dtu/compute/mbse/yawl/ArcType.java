659e82798048dc5d294d28d4082f31427719f1d8
TC06 <Cetin_tolga06@hotmail.com>
2017-05-15 21:30:03 +0200

    ukyiuloæp

--- dk.dtu.compute.mbse.tutorial.yawl/src/dk/dtu/compute/mbse/yawl/ArcType.java
+++ dk.dtu.compute.mbse.tutorial.yawl/src/dk/dtu/compute/mbse/yawl/ArcType.java
@@ -6,24 +6,24 @@ import org.pnml.tools.epnk.pnmlcoremodel.Attribute;
 
 /**
  * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Place Type</b></em>'.
+ * A representation of the model object '<em><b>Arc Type</b></em>'.
  * <!-- end-user-doc -->
  *
  * <p>
  * The following features are supported:
  * </p>
  * <ul>
- *   <li>{@link mbse.yawl.PlaceType#getText <em>Text</em>}</li>
+ *   <li>{@link mbse.yawl.ArcType#getText <em>Text</em>}</li>
  * </ul>
  *
- * @see mbse.yawl.YawlPackage#getPlaceType()
+ * @see mbse.yawl.YawlPackage#getArcType()
  * @model
  * @generated
  */
 public interface ArcType extends Attribute {
 	/**
 	 * Returns the value of the '<em><b>Text</b></em>' attribute.
-	 * The literals are from the enumeration {@link mbse.yawl.PType}.
+	 * The literals are from the enumeration {@link mbse.yawl.AType}.
 	 * <!-- begin-user-doc -->
 	 * <p>
 	 * If the meaning of the '<em>Text</em>' attribute isn't clear,
@@ -31,23 +31,23 @@ public interface ArcType extends Attribute {
 	 * </p>
 	 * <!-- end-user-doc -->
 	 * @return the value of the '<em>Text</em>' attribute.
-	 * @see mbse.yawl.PType
-	 * @see #setText(PType)
-	 * @see mbse.yawl.YawlPackage#getPlaceType_Text()
+	 * @see mbse.yawl.AType
+	 * @see #setText(AType)
+	 * @see mbse.yawl.YawlPackage#getArcType_Text()
 	 * @model
 	 * @generated
 	 */
 	AType getText();
 
 	/**
-	 * Sets the value of the '{@link mbse.yawl.PlaceType#getText <em>Text</em>}' attribute.
+	 * Sets the value of the '{@link mbse.yawl.ArcType#getText <em>Text</em>}' attribute.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @param value the new value of the '<em>Text</em>' attribute.
-	 * @see mbse.yawl.PType
+	 * @see mbse.yawl.AType
 	 * @see #getText()
 	 * @generated
 	 */
 	void setText(AType value);
 
-} // PlaceType
+} // ArcType
