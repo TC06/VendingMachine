1603e2a19bf1b7eff7a79399b4a4bbe83c3a576c
TC06 <Cetin_tolga06@hotmail.com>
2017-05-15 23:53:35 +0200

    rgthyj

--- dk.dtu.compute.mbse.tutorial.yawl.simulator/src/dk/dtu/compute/mbse/tutorial/yawl/simulator/application/SelectArcHandler.java
+++ dk.dtu.compute.mbse.tutorial.yawl.simulator/src/dk/dtu/compute/mbse/tutorial/yawl/simulator/application/SelectArcHandler.java
@@ -37,7 +37,7 @@ public class SelectArcHandler implements IActionHandler {
 
 	@Override
 	public boolean mousePressed(MouseEvent arg0, ObjectAnnotation annotation) {
-		/*if (annotation instanceof SelectArc) {
+		if (annotation instanceof SelectArc) {
 			SelectArc selectArc = (SelectArc) annotation;
 			// change status of the involved arc
 			selectArc.setSelected(!selectArc.isSelected());
@@ -74,78 +74,7 @@ public class SelectArcHandler implements IActionHandler {
 				return true;
 			}
 		}
-		return false;*/
-		if (annotation instanceof SelectArc) {
-			NetAnnotations netAnnotations = application.getNetAnnotations();
-			NetAnnotation current = netAnnotations.getCurrent();
-			SelectArc arcAnnotation = (SelectArc) annotation;
-			// change status of the involved arc
-			EnabledTransition srcTransition = arcAnnotation.getSourceTransition();
-			if (current.getObjectAnnotations().contains(srcTransition)) {
-				Object object = srcTransition.getObject();
-
-				if (object instanceof Transition) {
-					Transition transition = (Transition) object;
-					TType[] types = YAWLFunctions.getTransitionType(transition);
-					if (types[1] == TType.OR || types[1] == TType.XOR) {
-						getArcsList(srcTransition.getOutArcs(), arcAnnotation, transition);
-					} else {
-						arcAnnotation.setSelected(!arcAnnotation.isSelected());
-					}
-				}
-			}
-			EnabledTransition targetTransition = arcAnnotation.getTargetTransition();
-			if (current.getObjectAnnotations().contains(srcTransition)) {
-				Object object = srcTransition.getObject();
-
-				if (object instanceof Transition) {
-					Transition transition = (Transition) object;
-					TType[] types = YAWLFunctions.getTransitionType(transition);
-					if (types[0] == TType.XOR || types[0] == TType.OR) {
-						getArcsList(srcTransition.getInArcs(), arcAnnotation, transition);
-					} else {
-						arcAnnotation.setSelected(!arcAnnotation.isSelected());
-					}
-				}
-			}
-			application.update();
-
-			return true;
-		}
 		return false;
-	}
-	
-	public EList<SelectArc> getArcsList(EList<SelectArc> arcs, SelectArc arcAnnotation, Transition transition) {
-		TType[] types = YAWLFunctions.getTransitionType(transition);
-		if (arcAnnotation.isSelected()) {
-			boolean AtleasOneSelected = false;
-			if (arcs.size() > 1) {
-				arcAnnotation.setSelected(false);
-				for (SelectArc arc : arcs) {
-					if (arc.isSelected()) {
-						AtleasOneSelected = true;					
-						break;
-					}
-				}
-				if (!AtleasOneSelected) {
-					for (SelectArc arc : arcs) {
-						if (arc != arcAnnotation) {
-							arc.setSelected(true);
-							break;
-						}
-					}
-				}
-			}
-		} else if (types[1] == TType.OR || types[0] == TType.OR) {
-			arcAnnotation.setSelected(true);
-		} else if (types[1] == TType.XOR || types[0] == TType.XOR) {
-			for (SelectArc outArc : arcs) {
-				outArc.setSelected(false);
-			}
-			arcAnnotation.setSelected(true);
-		}
-		return arcs;
-	}
 
 	@Override
 	public boolean mouseReleased(MouseEvent arg0, ObjectAnnotation annotation) {
